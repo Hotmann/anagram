43a263685c1ca2333346d601a3ee3addace65191
Hotmann <hotmann442@gmail.com>
2022-05-28 16:37:53 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    if (sorted(word) == sorted(anagram)):
+        return True
+    else:
+        return False
+
+word = input("Enter the word: ")
+anagram = input("Enter the anagram: ")
+print(find_anagram(word, anagram))
+
